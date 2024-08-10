9af3ca5a92d96919a503a958e6e8820c06077935
Rafael <leafarbutzke@hotmail.com>
2024-08-10 17:10:23 -0300

    .

--- src/main2.c
+++ src/main2.c
@@ -10,78 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "data.h"
+#include "../includes/data.h"
 
-void putPixel(int sizeY,int sizeX, int posy, int posx, int rgb[4], mlx_image_t* image);
 void init_data(t_data *data);
-
-void pixel(int plaine[2], int pos[2], double angle, mlx_image_t* image)
-{
-	int collor;
-	int	index[2];
-	double coordWidth[2];
-
-	collor = ft_pixel(100, 0, 0, 255);
-	index[Y] = plaine[Y] +1;
-	while (--index[Y] >= 0)
-  	{
-    	index[X] = plaine[X] +1;
-    	while(--index[X] >= 0)
-		{
-			coordWidth[X] = round((double) (index[X] + pos[X] + index[Y]) * angle);
-			coordWidth[Y] = round((double) (index[X] + pos[X] - index[Y] ) * angle);
-			mlx_put_pixel(image, coordWidth[X], index[Y] + pos[Y], collor);
-			mlx_put_pixel(image, coordWidth[Y], index[Y] + pos[Y], collor);
-			coordWidth[X] = round((double) (index[X] + pos[X] + index[Y]) * angle /2);
-			coordWidth[Y] = round((double) (index[X] + pos[X] - index[Y] ) * angle / 2);
-			mlx_put_pixel(image, coordWidth[X] + (plaine[Y] / 3), index[Y] + pos[Y], collor);
-		}
-  	}
-}
-
-void putline(int plaine[2], int pos[2], mlx_image_t* image)
-{
-	int collor;
-	int y;
-	int x;
-
-	y = 0;
-	collor = ft_pixel(100, 0, 0, 255);
-	while (y < plaine[Y])
-	{
-		x = 0;
-		while(x < plaine[X])
-		{
-			mlx_put_pixel(image, pos[X] + x, pos[Y] + y, collor);
-			x++;
-		}
-		y++;  
-	}
-}
-
-void ray(t_data *data);
+void putPixel(int sizeY,int sizeX, int posy, int posx, int rgb[4], mlx_image_t* image);
 
 int main()
 {
 	t_data data;
 
 	init_data(&data);
-	mlx_image_to_window(data.mlx, data.image, 0, 0);
-	ray(&data);
-	mlx_image_to_window(data.mlx, data.player, (WIDTH / 2) - 200, HEIGHT /2);
+
+	for (int y = 0;y < mapWidth; y++)
+	{
+		for (int x= 0; x < mapWidth; x++)
+			printf("%d ", worldmap[y][x]);
+		printf("\n");
+	}
+/* 	mlx_image_to_window(data.mlx, data.image, 0, 0);
+
+
+
+
+
   	mlx_loop(data.mlx);
-	mlx_terminate(data.mlx);
-}
-
-void ray(t_data *data)
-{
-	int	plaine[2] = {150, 2};
-	int	pos[2] = {0, 0};
-
-	pixel(plaine, pos, 0.66, data->player);
-	pos[X] = 100;
-	pos[Y] = 0;
-	putline(plaine, pos, data->player);	
+	mlx_terminate(data.mlx); */
 }
 
 void init_data(t_data *data)
@@ -107,26 +60,27 @@ void init_data(t_data *data)
 	data->player_rgb[1] = 0;
 	data->player_rgb[2] = 0;
 	data->player_rgb[3] = 255;
+
 	putPixel(HEIGHT /2, WIDTH, 0, 0, data->ceiling_rgb, data->image);
 	putPixel(HEIGHT /2, WIDTH, HEIGHT /2, 0, data->floor_rgb, data->image);
 }
 
 void putPixel(int sizeY,int sizeX, int posy, int posx, int rgb[4], mlx_image_t* image)
 {
-  int collor;
-  int y;
-  int x;
+	int collor;
+	int y;
+	int x;
 
-  y = 0;
-  collor = ft_pixel(rgb[0], rgb[1], rgb[2], rgb[3]);
+	y = 0;
+	collor = ft_pixel(rgb[0], rgb[1], rgb[2], rgb[3]);
 	while (y < sizeY)
-  {
-    x = 0;
-    while(x < sizeX)
-    {
+	{
+		x = 0;
+		while(x < sizeX)
+		{
 			mlx_put_pixel(image, posx + x, posy + y, collor);
-      x++;
-    }
-    y++;  
-  }
+			x++;
+		}
+		y++; 
+	}
 }
--- src/main_marcal.c
+++ src/main_marcal.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_Marcal.c                                      :+:      :+:    :+:   */
+/*   main_marcal.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: rbutzke <rbutzke@student.42so.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 07:48:30 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/08/09 15:15:29 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/08/10 16:01:53 by rbutzke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include "../lib/lib_get_print/includes/libft.h"
 #include "../lib/MLX42/include/MLX42/MLX42.h"
+#include <stdio.h>
 #define WIDTH 800
 #define HEIGHT 600
 #define mapWidth 24
 #define mapHeight 24
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+#define WHITE 3
+#define YELOW 4
 
 typedef struct s_game {
 	// em qual quadrado o player está posicionado no mapa;
@@ -96,41 +104,36 @@ int	ft_pixel(int r, int g, int b, int a)
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-// rendering ceiling and floor
+void putPixel(int sizeY,int sizeX, int posy, int posx, int rgb[4], mlx_image_t* image)
+{
+	int collor;
+	int y;
+	int x;
+
+	y = 0;
+	collor = ft_pixel(rgb[0], rgb[1], rgb[2], rgb[3]);
+	while (y < sizeY)
+	{
+		x = 0;
+		while(x < sizeX)
+		{
+			mlx_put_pixel(image, posx + x, posy + y, collor);
+			x++;
+		}
+		y++;
+	}
+}
+
 int	rendering_ceiling_and_floor(t_game *game)
 {
-	game->screen_img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
-	if (!game->screen_img)
-	{
-		ft_putstr_fd((char *) mlx_strerror(mlx_errno), STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
-	for (uint32_t i = 0; i < game->screen_img->width; ++i)
-	{
-		for (uint32_t y = 0; y < (game->screen_img->height / 2); ++y)
-		{
-			uint32_t color = ft_pixel(
-				0xFF, // R
-				0xFF, // G
-				0xFF, // B
-				0xFF  // A
-			);
-			mlx_put_pixel(game->screen_img, i, y, color);
-		}
-	}
-	for (uint32_t i = 0; i < game->screen_img->width; ++i)
-	{
-		for (uint32_t y = (game->screen_img->height / 2); y < game->screen_img->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				0x42, // R
-				0xFF, // G
-				0x64, // B
-				0xD9  // A
-			);
-			mlx_put_pixel(game->screen_img, i, y, color);
-		}
-	}
+	int floor_rgb[4] = {0, 100, 0, 255};
+	int	ceiling_rgb[4] = {0, 0, 100, 255};
+	int	zero[4] = {0, 0, 0, 255};
+
+	ft_memset(game->screen_img->pixels, 0, HEIGHT * WIDTH);
+	putPixel(HEIGHT /2, WIDTH, 0, 0, zero, game->screen_img);
+	putPixel(HEIGHT, WIDTH, 0, 0, floor_rgb, game->screen_img);
+	putPixel(HEIGHT /2, WIDTH, 0, 0, ceiling_rgb, game->screen_img);
 	if (mlx_image_to_window(game->mlx, game->screen_img, 0, 0) == -1)
 	{
 		mlx_close_window(game->mlx);
@@ -203,9 +206,9 @@ int	first_step(t_game *game)
 	return (EXIT_SUCCESS);
 }
 
-int	instatiation_window(mlx_t *mlx)
+int	instatiation_window(mlx_t *mlx, t_game *main)
 {
-	mlx_key_hook(mlx, loop_hook_function, mlx);
+	mlx_key_hook(mlx, loop_hook_function, (void*)main);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
@@ -215,99 +218,130 @@ double	abs_double(double number)
 {
 	if (number < 0)
 		return (-number);
+	return (number);
+}
+
+int		defineColor(t_game *main_struct)
+{
+	int allcolor[6];
+	int color;
+
+	// int alpha = 1;
+	allcolor[RED] = ft_pixel(255, 0, 0, 1);
+	allcolor[GREEN] = ft_pixel(0, 255, 0, 1);
+	allcolor[BLUE] = ft_pixel(0, 0, 255, 1);
+	allcolor[WHITE] = ft_pixel(0, 0, 0, 1);
+	allcolor[YELOW] = ft_pixel(255, 255, 224, 1);
+	switch (worldMap[main_struct->mapX][main_struct->mapY])
+	{
+		case 1: color = allcolor[RED]; break;
+		case 2: color = allcolor[GREEN]; break;
+		case 3: color = allcolor[BLUE]; break;
+		case 4: color = allcolor[WHITE]; break;
+		default: color = allcolor[YELOW]; break;
+	}
+	if (main_struct->side == 1)
+			color = color / 2;
+	return (color);
+}
+
+void	dda(t_game *main_struct)
+{
+	main_struct->deltaDistX = fabs(1 / main_struct->rayDirX);
+	main_struct->deltaDistY = fabs(1 / main_struct->rayDirY);
+	main_struct->hit = 0;
+	while (main_struct->hit == 0)
+	{
+		if (main_struct->sideDistX < main_struct->sideDistY)
+		{
+			main_struct->sideDistX += main_struct->deltaDistX;
+			main_struct->mapX += main_struct->stepX;
+			main_struct->side = 0;
+			}
+		else
+		{
+			main_struct->sideDistY += main_struct->deltaDistY;
+			main_struct->mapY += main_struct->stepY;
+			main_struct->side = 1;
+		}
+		if (worldMap[main_struct->mapX][main_struct->mapY] > 0)
+			main_struct->hit = 1;
+	}
+	if (main_struct->side == 0) 
+		main_struct->perpWallDist = (main_struct->sideDistX - main_struct->deltaDistX);
 	else
-		return (number);
+		main_struct->perpWallDist = (main_struct->sideDistY - main_struct->deltaDistY);
+}
+
+void draw(int width, t_game *main_struct)
+{
+	int color = defineColor(main_struct);
+	int row = main_struct->drawStart;
+	while (row < main_struct->drawEnd)
+	{
+		mlx_put_pixel(main_struct->screen_img, width, row, color);
+		row++;
+	}
+	main_struct->moveSpeed = 100;
+	main_struct->rotSpeed = 50;	
+}
+
+void side(t_game *main_struct)
+{
+	if (main_struct->rayDirX < 0)
+	{
+		main_struct->stepX = -1;
+		main_struct->sideDistX = (main_struct->posX - main_struct->mapX) * main_struct->deltaDistX;
+	}
+	else
+	{
+		main_struct->stepX = 1;
+		main_struct->sideDistX = (main_struct->mapX + 1.0 - main_struct->posX) * main_struct->deltaDistX;
+	}
+	if (main_struct->rayDirY < 0)
+	{
+		main_struct->stepY = -1;
+		main_struct->sideDistY = (main_struct->posY - main_struct->mapY) * main_struct->deltaDistY;
+	}
+	else
+	{
+		main_struct->stepY = 1;
+		main_struct->sideDistY = (main_struct->mapY + 1.0 - main_struct->posY) * main_struct->deltaDistY;
+	}	
+}
+
+void	init_value(int x, t_game *main_struct)
+{
+	// coordenada x no espaço da câmera
+	main_struct->cameraX = 2 * x / (double)WIDTH -1;
+
+	// direção dos raios X e Y
+	main_struct->rayDirX = main_struct->dirX + main_struct->planeX * main_struct->cameraX;
+	main_struct->rayDirY = main_struct->dirY + main_struct->planeY * main_struct->cameraX;
+	main_struct->mapX = (int) main_struct->posX;
+	main_struct->mapY = (int) main_struct->posY;
+/* 	main_struct->deltaDistX = fabs(1 / main_struct->rayDirX);
+	main_struct->deltaDistY = fabs(1 / main_struct->rayDirY); */
 }
 
 // raycasting loop
 void	raycasting(t_game *main_struct)
 {
-	for (int x = 0; x < WIDTH; x++)
+	first_step(main_struct);
+	for (int x = 1; x < WIDTH -1; x++)
 	{
-		// coordenada x no espaço da câmera
-		main_struct->cameraX = 2 * x / (double)WIDTH - 1;
-		// direção dos raios X e Y
-		main_struct->rayDirX = main_struct->dirX + main_struct->planeX * main_struct->cameraX;
-		main_struct->rayDirY = main_struct->dirY + main_struct->planeY * main_struct->cameraX;
-		main_struct->mapX = (int) main_struct->posX;
-		main_struct->mapY = (int) main_struct->posY;
-		
-		main_struct->deltaDistX = (main_struct->rayDirX == 0) ? 1e30 : abs_double(1 / main_struct->rayDirX);
-		main_struct->deltaDistY = (main_struct->rayDirY == 0) ? 1e30 : abs_double(1 / main_struct->rayDirY);
+		init_value(x, main_struct);
+		side(main_struct);
+		dda(main_struct);
 
-		main_struct->hit = 0;
-		if (main_struct->rayDirX < 0)
-		{
-			main_struct->stepX = -1;
-			main_struct->sideDistX = (main_struct->posX - main_struct->mapX) * main_struct->deltaDistX;
-		}
-		else
-		{
-			main_struct->stepX = 1;
-			main_struct->sideDistX = (main_struct->mapX + 1.0 - main_struct->posX) * main_struct->deltaDistX;
-		}
-		if (main_struct->rayDirY < 0)
-		{
-			main_struct->stepY = -1;
-			main_struct->sideDistY = (main_struct->posY - main_struct->mapY) * main_struct->deltaDistY;
-		}
-		else
-		{
-			main_struct->stepY = 1;
-			main_struct->sideDistY = (main_struct->mapY + 1.0 - main_struct->posY) * main_struct->deltaDistY;
-		}
-		// perform DDA
-		while (main_struct->hit == 0)
-		{
-			if (main_struct->sideDistX < main_struct->sideDistY)
-			{
-				main_struct->sideDistX += main_struct->deltaDistX;
-				main_struct->mapX += main_struct->stepX;
-				main_struct->side = 0;
-			}
-			else
-			{
-				main_struct->sideDistY += main_struct->deltaDistY;
-				main_struct->mapY += main_struct->stepY;
-				main_struct->side = 1;
-			}
-			if (worldMap[main_struct->mapX][main_struct->mapY] > 0) main_struct->hit = 1;
-		}
-		if (main_struct->side == 0) main_struct->perpWallDist = (main_struct->sideDistX - main_struct->deltaDistX);
-		else main_struct->perpWallDist = (main_struct->sideDistY - main_struct->deltaDistY);
-		
 		main_struct->lineHeight = (int) (HEIGHT / main_struct->perpWallDist);
-
 		main_struct->drawStart = -main_struct->lineHeight / 2 + HEIGHT / 2;
-		if (main_struct->drawStart < 0) main_struct->drawStart = 0;
+		if (main_struct->drawStart < 0)
+			main_struct->drawStart = 0;
 		main_struct->drawEnd = main_struct->lineHeight / 2 + HEIGHT / 2;
-		if (main_struct->drawEnd >= HEIGHT) main_struct->drawEnd = HEIGHT -1;
-
-		// int alpha = 1;
-		int red = ft_pixel(255, 0, 0, 1);
-		int green = ft_pixel(0, 255, 0, 1);
-		int blue = ft_pixel(0, 0, 255, 1);
-		int white = ft_pixel(0, 0, 0, 1);
-		int	yellow = ft_pixel(255, 255, 224, 1);
-		int color;
-		switch (worldMap[main_struct->mapX][main_struct->mapY])
-		{
-			case 1: color = red; break; // red color
-			case 2: color = green; break; // green color
-			case 3: color = blue; break; // blue color
-			case 4: color = white; break; // white color
-			default: color = yellow; break; // yellow color
-		}
-		
-		if (main_struct->side == 1) {color = color / 2;}
-		int row = main_struct->drawStart;
-		while (row < main_struct->drawEnd)
-		{
-			mlx_put_pixel(main_struct->screen_img, x, row, color);
-			row++;
-		}
-		main_struct->moveSpeed = 100;
-		main_struct->rotSpeed = 50;
+		if (main_struct->drawEnd >= HEIGHT)
+			main_struct->drawEnd = HEIGHT -1;
+		draw(x, main_struct);
 	}
 }
 // movement and raycasting
@@ -318,6 +352,7 @@ int	main(void)
 	main_struct.mlx = mlx_init(WIDTH, HEIGHT, "cub3D", true);
 	if (!main_struct.mlx)
 		return (EXIT_FAILURE);
+	main_struct.screen_img = mlx_new_image(main_struct.mlx, WIDTH, HEIGHT);
 	first_step(&main_struct);
 	
 	// vetor de posição do player
@@ -326,15 +361,15 @@ int	main(void)
 	main_struct.dirX = -1, main_struct.dirY = 0;
 	// versão 2D raycaster do plano da câmera
 	main_struct.planeX = 0, main_struct.planeY = 0.66;
-	
+
 	// tempo atual do frame
 	main_struct.time = 0;
 	// tempo anterior do frame
 	main_struct.oldtime = 0;
-	
+
 	// mlx_image_t *render_line = mlx_new_image(mlx, WIDTH, HEIGHT);
 	raycasting(&main_struct);
 	mlx_image_to_window(main_struct.mlx, main_struct.screen_img, 0, 0);
-	instatiation_window(main_struct.mlx);
+	instatiation_window(main_struct.mlx, &main_struct);
 	return (EXIT_SUCCESS);
 }
